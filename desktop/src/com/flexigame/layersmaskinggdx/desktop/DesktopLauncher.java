3aa08e209694ebb62f8c03231ec4426300460206
Radosław Gniazdowski <7131876+rgniazdowski@users.noreply.github.com>
2016-11-16 07:19:04 +0100

    Changed DesktopLauncher

--- desktop/src/com/flexigame/layersmaskinggdx/desktop/DesktopLauncher.java
+++ desktop/src/com/flexigame/layersmaskinggdx/desktop/DesktopLauncher.java
@@ -5,8 +5,11 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.flexigame.layersmaskinggdx.MyGdxLayersMaskingEffect;
 
 public class DesktopLauncher {
-	public static void main (String[] arg) {
-		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
-		new LwjglApplication(new MyGdxLayersMaskingEffect(), config);
-	}
-}
+    public static void main(String[] arg) {
+        LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+        config.width = 576;
+        config.height = 1024;
+        config.title = "Layers Masking Effect X - flexigame.com - Technical Blog";
+        new LwjglApplication(new MyGdxLayersMaskingEffect(), config);
+    }
+} // class DesktopLauncher
